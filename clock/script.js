92175b4afe9cd69e42e32367c3a1879b181cd017
Sangeeta Mishra <121925088+sangeeta2003@users.noreply.github.com>
2023-09-05 17:35:09 +0530

    Add files via upload

---
+++ clock/script.js
@@ -0,0 +1,32 @@
+setInterval(showTime,1000);
+// defining function
+function showTime(){
+    let time = new Date();
+    let hour = time . getHours();
+    let min = time . getMinutes();
+    let sec = time . getSeconds();
+am_pm = "AM";
+    
+
+
+// in 12hrs format
+if(hour <= 12){
+    if( hour > 12){
+        hour -=12;
+        am_pm = "PM";
+    }else if(hour = 0){
+        hr = 12;
+        am_pm = "AM";
+    }
+}
+
+    hour = hour < 10 ? "0" + hour : hour;
+    min = min<10 ? "0" + min : min;
+    sec = sec<10 ? "0" + sec: sec;
+    let currentTime = hour + ":" + min + ":" + sec + am_pm;
+
+    // display the time
+    document.getElementById("value").innerHTML = currentTime;
+}
+showTime();
+
